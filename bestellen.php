5453c8823a64dc13df2e31bc0d25330b9f7921d6
lekkerding01 <Bramcorbeij@gmail.com>
2025-05-09 09:33:03 +0200

    Create bestellen.php

---
+++ bestellen.php
@@ -0,0 +1,106 @@
+
+.p3 {
+    font-size: 13.5px;
+    text-align: left;
+    font-family: fantasy;
+}
+
+.p2 {
+    font-size: 19px;
+    font-family: fantasy;
+    font-weight: bold;
+    padding-top: 25px;
+    padding-bottom: 25px;
+    text-align: center;
+  }
+
+  .p2:hover {
+    background-color: rgb(99, 63, 0);
+    color: whitesmoke;
+    border-radius: 20px;
+    padding-top: 25px;
+    padding-bottom: 25px;
+  }
+
+.p1 {
+    font-size: 15px;
+    font-family: fantasy;
+    text-align: left;
+    padding-top: 3px;
+    
+  }
+    .p1:hover {
+    background-color: rgb(99, 63, 0);
+    color: whitesmoke;
+    border-radius: 20px;
+    padding-top: 3px;
+    
+    }
+
+
+
+
+
+
+
+
+    .linksbreder {
+        padding-left: 10px;
+        top: -5px;
+        position: relative;
+    }
+    .zendknop {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    background-color: #ffcf60;
+    border-radius: 10px;
+    margin-top: 20px;
+  }
+  input[type=submit] {
+    margin: 10px;
+    background-color: #ffbf38;
+    border: 3px, solid #b36300;
+    border-radius: 10px;
+    padding: 10px;
+    width: 200px;
+  }
+
+  input[type=submit]:hover {
+    cursor: pointer;
+  }
+
+  .contact-links {
+    display: flex;
+    flex-direction: row;
+    justify-content: left;
+    background-color: #ffcf60;
+    border-radius: 10px;
+    margin-top: 20px;
+  }
+  
+.contact-links > div {
+    background-color: #ffbf38;
+    border: 3px, solid #b36300;
+    border-radius: 30px;
+    width: auto;
+    min-height: 160px;
+    margin: 10px;
+    padding: 50px;
+    text-align: left;
+    line-height: 40px;
+    font-weight: bold;
+  }
+.icon {
+    width: 20px;
+    top: 3.5px;
+    position: relative;
+    margin-right: 10px;
+    
+}
+.acontact {
+    text-decoration: none;
+    color: black;
+}
+
+
