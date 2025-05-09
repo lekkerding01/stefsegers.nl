c375a0ec34cacca61440152b2553f8def88a6181
lekkerding01 <Bramcorbeij@gmail.com>
2025-05-09 09:40:36 +0200

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="header">
+<img class="logo" src="../PO/logotekst.png"/>
+<a href="overons.php">Over ons</a>
+<a class="actief">Contact</a>
+<a href="bestellen.php">Bestellen</a>
+<a href="home.php">Home</a>
+</div>
+<div class="contact-links">
+    <div>
+    <a class="acontact" href="tel:+31 13 987 6543">Tel: +31 13 987 6543</a><br />
+    <a class="acontact" href="mailto:info@flippingdelicious.nl">Email: info@flippingdelicious.nl</a><br />
+    <a class="acontact" href="home.php">Website: www.flippingdelicious.nl</a><br />
+    <a>Adres: Pancake Plaza 15, 5011 CX Tilburg</a>
+</div>
+</div>
+</body>
+
